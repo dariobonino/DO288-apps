d5bf084305184f0558bbdffae383d5903cc59ac1
dario.bonino <dario.bonino@bonino.net>
2024-02-23 12:37:57 +0100

    ok

--- simulazione/test-finish.sh
+++ simulazione/test-finish.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+mkdir -p ~/test/deleteme
+cd ~/test/deleteme
+git clone https://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@github.com/dariobonino/DO288-apps
+cd ~/test/deleteme/DO288-apps
+git checkout dario
+git pull
+git branch -d temporary
+git push -d origin temporary
+cd ~
+rm -Rf ~/test/deleteme
 
 podman kill $(podman ps -q)
 podman rm $(podman ps -aq)
